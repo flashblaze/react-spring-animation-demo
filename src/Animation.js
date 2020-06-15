c01bcee4455dd3407bec4e3fa72d9a7cbd101a2c
FlashBlaze <neerajlagwankar@gmail.com>
2020-06-15 19:30:33 +0530

    Circle animation

--- src/Animation.js
+++ src/Animation.js
@@ -1,25 +1,47 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import { Spring } from 'react-spring/renderprops';
 
 const Animation = () => {
   const pathRef = useRef();
+  const [offset, setOffset] = useState(null);
 
   useEffect(() => {
-    console.log(pathRef.current.getTotalLength());
-  }, []);
+    setOffset(pathRef.current.getTotalLength());
+  }, [offset]);
 
   return (
     <div>
-      <svg>
-        <circle
-          strokeWidth="3"
-          cx="100"
-          cy="50"
-          r="40"
-          stroke="black"
-          fill="none"
-          ref={pathRef}
-        />
-      </svg>
+      {offset ? (
+        <Spring from={{ x: offset }} to={{ x: 0 }}>
+          {(props) => (
+            <svg>
+              <circle
+                strokeDashoffset={props.x}
+                strokeDasharray={offset}
+                strokeWidth="3"
+                cx="100"
+                cy="50"
+                r="40"
+                stroke="black"
+                fill="none"
+                ref={pathRef}
+              />
+            </svg>
+          )}
+        </Spring>
+      ) : (
+        <svg>
+          <circle
+            strokeWidth="3"
+            cx="100"
+            cy="50"
+            r="40"
+            stroke="none"
+            fill="none"
+            ref={pathRef}
+          />
+        </svg>
+      )}
     </div>
   );
 };
