9f7bb7781a865860e4f59303a2dd576155039721
FlashBlaze <neerajlagwankar@gmail.com>
2020-06-15 19:30:08 +0530

    Simple circle svg

--- src/Animation.js
+++ src/Animation.js
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
-export const Animation = () => {
-  return <h1>Hello World!</h1>;
+const Animation = () => {
+  const pathRef = useRef();
+
+  useEffect(() => {
+    console.log(pathRef.current.getTotalLength());
+  }, []);
+
+  return (
+    <div>
+      <svg>
+        <circle
+          strokeWidth="3"
+          cx="100"
+          cy="50"
+          r="40"
+          stroke="black"
+          fill="none"
+          ref={pathRef}
+        />
+      </svg>
+    </div>
+  );
 };
+
+export default Animation;
